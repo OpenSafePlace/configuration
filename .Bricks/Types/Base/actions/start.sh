6af23475e9b2211614c7b6b3f2dc919326f03cb0
Artur Shkadzinski <real-xihnik@yandex.by>
2023-03-23 01:43:38 +0300

    feat: added xauthority protection script to startup

--- .Bricks/Types/Base/actions/start.sh
+++ .Bricks/Types/Base/actions/start.sh
@@ -3,3 +3,6 @@
 # General process
 ## Configure firewall rules
 . $project_main_folder/.Bricks/Containers/Base/$global__project_os/$global__project_os_version/Firewall/Open/10100.sh
+##
+## Update access
+. $project_main_folder/.Bricks/Containers/Base/$global__project_os/$global__project_os_version/Protect/xauthority.sh
