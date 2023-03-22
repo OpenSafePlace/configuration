23722c77e67434214f702cc8a80a080f19d3d390
Artur Shkadzinski <real-xihnik@yandex.by>
2023-03-23 01:23:33 +0300

    feat: improved firewall support in Linux

--- .Bricks/Containers/Base/Fedora/37/Setup/iptables.sh
+++ .Bricks/Containers/Base/Fedora/37/Setup/iptables.sh
@@ -2,6 +2,6 @@
 
 # General process
 ## Setup package
-inside 1 root base "dnf install -y iptables"
+inside 1 root base "dnf install -y iptables-nft"
 ##
 info "В контейнере 'Base' настроен пакет 'iptables'"
