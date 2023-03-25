1471281afe86eb0d616e4569b4570a3a297e3eb8
Artur Shkadzinski <real-xihnik@yandex.by>
2023-03-25 03:19:29 +0300

    feat: added wayland support

--- .Bricks/Dependencies/stop-use-gui.sh
+++ .Bricks/Dependencies/stop-use-gui.sh
@@ -12,8 +12,17 @@ pkill XQuartz Xquartz >&1
 xhost - >&1
 }
 ##
-## Stop use GUI (linux)
+## Stop use GUI (linux, x11)
 [[ $global__project_host_os_type == "linux" ]] &&
-[[ $global__project_gui == true ]] && {
+[[ $global__project_gui == true ]] &&
+[[ $XDG_SESSION_TYPE == "x11" ]] && {
+xhost - >&1
+}
+##
+## Stop use GUI (linux, wayland)
+[[ $global__project_host_os_type == "linux" ]] &&
+[[ $global__project_gui == true ]] &&
+[[ $XDG_SESSION_TYPE == "wayland" ]] && {
+xhost - SI:localuser:${global__project_host_user} >&1
 xhost - >&1
 }
--- .Bricks/Dependencies/use-gui.sh
+++ .Bricks/Dependencies/use-gui.sh
@@ -12,8 +12,17 @@ pkill XQuartz Xquartz >&1
 xhost - >&1
 }
 ##
-## Use GUI (linux)
+## Use GUI (linux, x11)
 [[ $global__project_host_os_type == "linux" ]] &&
-[[ $global__project_gui == true ]] && {
+[[ $global__project_gui == true ]] &&
+[[ $XDG_SESSION_TYPE == "x11" ]] && {
+xhost - >&1
+}
+##
+## Use GUI (linux, wayland)
+[[ $global__project_host_os_type == "linux" ]] &&
+[[ $global__project_gui == true ]] &&
+[[ $XDG_SESSION_TYPE == "wayland" ]] && {
+xhost + SI:localuser:${global__project_host_user} >&1
 xhost - >&1
 }
