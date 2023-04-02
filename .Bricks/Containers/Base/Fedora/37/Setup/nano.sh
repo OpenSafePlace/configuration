078c249ab2d7585c42533001419ef8da7746fca8
Artur Shkadzinski <real-xihnik@yandex.by>
2023-04-02 23:38:31 +0300

    feat: configure launch of nano

--- .Bricks/Containers/Base/Fedora/37/Setup/nano.sh
+++ .Bricks/Containers/Base/Fedora/37/Setup/nano.sh
@@ -4,4 +4,7 @@
 ## Setup package
 inside 1 root base "dnf install -y nano"
 ##
+## Configure launch
+inside 1 public base "echo 'alias nano=\"nano --linenumbers --mouse --softwrap\"' >> /home/public/.zsh_alias"
+##
 info "В контейнере 'Base' настроен пакет 'nano'"
