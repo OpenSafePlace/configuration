776e52186b94d2776e99957f8ce99c43d2ec5d58
Artur Shkadzinski <real-xihnik@yandex.by>
2023-04-02 23:38:08 +0300

    fix: changed container main folders logic

--- .Bricks/Containers/Base/Fedora/37/Users/public.sh
+++ .Bricks/Containers/Base/Fedora/37/Users/public.sh
@@ -5,12 +5,11 @@
 inside 1 root base "useradd -m public"
 ##
 ## Create project directories and links
-inside 1 root base "chmod 777 /project"
-inside 1 public base "mkdir -p /project/files"
-inside 1 root base "chmod 700 /project/files"
-inside 1 root base "chmod 700 /project/bridge"
-inside 1 public base "cd /home/public && ln -s /project project"
+inside 1 public base "mkdir -p /home/public/project"
+inside 1 public base "chmod 700 /home/public/project"
 inside 1 public base "mkdir -p /home/public/other"
+inside 1 public base "chmod 700 /home/public/other"
+inside 1 public base "ln -s /bridge /home/public/bridge"
 ##
 ## Set password
 inside 1 root base "echo 'public:${global__project_keys[0]}' | chpasswd"
