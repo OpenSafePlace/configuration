221009127ef17ae37bcfe2c66e4a58225c3788cb
Artur Shkadzinski <real-xihnik@yandex.by>
2023-04-16 00:35:18 +0300

    feat: added python configuration from base

---
+++ .Bricks/Types/Python/actions/protect.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# General process
+## Create new keys
+global__project_keys_new+=($(openssl rand -base64 33) "['public'] + ['sudo']")
+##
+## Replace keys
+. $global__project_main_folder/.Bricks/Containers/Python/$global__project_os/$global__project_os_version/Protect/sudo.sh
---
+++ .Bricks/Types/Python/actions/update-params.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# General process
+## Get power percent
+[[ $global__project_power == "highest" ]] &&
+local__project_power_percent=$(($global__project_cpu_cores_count * 100))
+##
+[[ $global__project_power == "high" ]] &&
+local__project_power_percent=$(($global__project_cpu_cores_count * (1 * 100 / 3 * 3)))
+##
+[[ $global__project_power == "medium" ]] &&
+local__project_power_percent=$(($global__project_cpu_cores_count * (1 * 100 / 3 * 2)))
+##
+[[ $global__project_power == "low" ]] &&
+local__project_power_percent=$(($global__project_cpu_cores_count * (1 * 100 / 3 * 1)))
+##
+[[ $global__project_power == "lowest" ]] &&
+local__project_power_percent=$(($global__project_cpu_cores_count * 8))
+##
+## Update params (container: python)
+inside 1 public python "echo -n '' > /home/public/.zsh_params"
+inside 1 public python "echo -n 'export global__project_power=$local__project_power_percent' >> /home/public/.zsh_params"
+##
+## Unset local vars
+unset local__project_power_percent
