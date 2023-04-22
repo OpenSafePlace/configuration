863119f1310e222f9d09ba7eb2503718f535b646
Artur Shkadzinski <real-xihnik@yandex.by>
2023-04-23 00:16:41 +0300

    feat: added code configuration from base

---
+++ .Bricks/Types/Code/actions/upgrade-minor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# General process
+## Upgrade minor
+. $global__project_main_folder/.Bricks/Containers/Code/$global__project_os/$global__project_os_version/Repositories/upgrade-minor.sh
