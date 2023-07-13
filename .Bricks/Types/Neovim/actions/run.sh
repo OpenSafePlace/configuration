f50db249db7ea6f0bd9d0c0e49ae6c420cc660c0
Artur Shkadzinski <real-xihnik@yandex.by>
2023-07-13 22:29:13 +0300

    feat: added neovim configuration from base

---
+++ .Bricks/Types/Neovim/actions/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# General process
+## Options for start the main internal processes
+info "Отсутствуют варианты запуска внутренних процессов в конфигурации проекта"
---
+++ .Bricks/Types/Neovim/actions/upgrade-minor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# General process
+## Upgrade minor
+. $global__project_main_folder/.Bricks/Containers/Neovim/$global__project_os/$global__project_os_version/Repositories/upgrade-minor.sh
