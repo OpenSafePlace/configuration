12eb774f049dff2602bdb5e84d6f298c01b02d70
Artur Shkadzinski <real-xihnik@yandex.by>
2023-08-27 02:02:57 +0300

    feat(types->neovim->enter): removed info message

--- .Bricks/Types/Neovim/actions/enter.sh
+++ .Bricks/Types/Neovim/actions/enter.sh
@@ -17,10 +17,6 @@ case $choice in
     ##
     space
     ##
-    info "Популярные команды: sudo, dnf, nano, git, htop, du, catimg"
-    ##
-    space
-    ##
     ## Enter in container
     inside 10 public neovim >&3
     ;;
