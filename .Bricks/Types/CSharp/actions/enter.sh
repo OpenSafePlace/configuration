b829bde0962ba0c9db092b709135639d00ae907a
Artur Shkadzinski <real-xihnik@yandex.by>
2023-08-27 02:01:49 +0300

    feat(types->csharp->enter): removed info message

--- .Bricks/Types/CSharp/actions/enter.sh
+++ .Bricks/Types/CSharp/actions/enter.sh
@@ -17,10 +17,6 @@ case $choice in
     ##
     space
     ##
-    info "Популярные команды: sudo, dnf, nano, git, htop, du, catimg, dotnet"
-    ##
-    space
-    ##
     ## Enter in container
     inside 10 public csharp >&3
     ;;
