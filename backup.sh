00a3a1e69c24084d5e127f6ccba4c25c13bdc99d
Artur Shkadzinski <real-xihnik@yandex.by>
2023-09-24 23:02:28 +0300

    feat: added backup functionality

---
+++ backup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# General process
+## There should be no '/' at the end
+global__project_main_folder="."
+##
+## Create regular folders
+. $global__project_main_folder/.Bricks/Dependencies/regular-folders.sh
+##
+## Output threads
+. $global__project_main_folder/.Bricks/Dependencies/use-log.sh
+##
+## Messages types set
+. $global__project_main_folder/.Bricks/Dependencies/types-set-message.sh
+##
+## Check installed packages
+. $global__project_main_folder/.Bricks/Dependencies/check-packages.sh
+##
+## Get project config
+. $global__project_main_folder/.Bricks/Dependencies/config-get.sh
+##
+## Check running project
+. $global__project_main_folder/.Bricks/Dependencies/check-project.sh
+##
+## Execute types set
+. $global__project_main_folder/.Bricks/Dependencies/types-set-execute.sh
+##
+## Show current date & time
+info "$(date '+%H:%M:%S (%d/%m/%Y)')"
+##
+space
+##
+## Command confirmation
+input "Вы точно хотите создать бекап проекта? (y/n)\n"
+input "=> "
+##
+read -p '' choice
+##
+space
+##
+case $choice in
+"y")
+    continue
+    ;;
+*)
+    info "Процесс остановлен"
+    ##
+    ## Message exit
+    . $global__project_main_folder/.Bricks/Dependencies/message-exit.sh
+    ##
+    ## Exit with code
+    exit 0
+    ;;
+esac
+##
+## Create backup
+. $global__project_main_folder/.Bricks/Types/$global__project_type/actions/backup.sh
+##
+space
+##
+info "Бекап создан"
+##
+## Message exit
+. $global__project_main_folder/.Bricks/Dependencies/message-exit.sh
+##
+## Exit with code
+exit 0
