a51daa34f7370dbbcebae9162af71f88cacebfe2
Artur Shkadzinski <real-xihnik@yandex.by>
2023-09-25 23:39:01 +0300

    feat(types->csharp): added backup functionality

---
+++ .Bricks/Types/CSharp/actions/backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# General process
+## Vars
+local__datetime_iso=$(date -u +%Y-%m-%dT%H:%M:%S.%3N%:z)
+local__archive_key=$(openssl rand -base64 48)
+##
+## Create backup
+. $global__project_main_folder/.Bricks/Containers/CSharp/$global__project_os/$global__project_os_version/Backups/archive.sh
+##
+## Info
+info "Ключ => '${local__archive_key}'"
+##
+## Security
+sh -c "chmod 770 -R $global__project_main_folder/Backups/*"
+sh -c "chown \$(whoami):docker -R $global__project_main_folder/Backups/*"
+##
+## Unset local vars
+unset local__datetime_iso
+unset local__archive_key
