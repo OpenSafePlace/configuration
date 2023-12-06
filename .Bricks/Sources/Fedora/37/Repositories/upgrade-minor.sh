f7a9ce4c18c43e61c01576c7764bddd0f991710e
Artur Shkadzinski <real-xihnik@yandex.by>
2023-12-06 22:52:34 +0300

    fix(sources->f37): upgrade minor

--- .Bricks/Sources/Fedora/37/Repositories/upgrade-minor.sh
+++ .Bricks/Sources/Fedora/37/Repositories/upgrade-minor.sh
@@ -5,6 +5,6 @@
 local__container_name_lowercase="${local__container_name,,}"
 ##
 ## Minor upgrade OS
-inside 1 $local__container_name_lowercase root "dnf upgrade -y"
+inside 1 root $local__container_name_lowercase "dnf upgrade -y"
 ##
 info "В контейнере '$local__container_name' все пакеты обновлены"
