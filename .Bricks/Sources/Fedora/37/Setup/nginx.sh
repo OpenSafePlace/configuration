524340194f149a1749cfc19158622e68d9f75db9
Artur Shkadzinski <real-xihnik@yandex.by>
2023-12-06 22:37:41 +0300

    fix(sources->f37): hid the keys in an external logs

--- .Bricks/Sources/Fedora/37/Setup/nginx.sh
+++ .Bricks/Sources/Fedora/37/Setup/nginx.sh
@@ -8,7 +8,6 @@ local__container_name_lowercase="${local__container_name,,}"
 inside 1 root $local__container_name_lowercase "dnf install -y nginx"
 ##
 ## Configure package
-inside 1 root $local__container_name_lowercase "mkdir /var/log/nginx"
 inside 1 root $local__container_name_lowercase "touch /var/log/nginx/access.log"
 inside 1 root $local__container_name_lowercase "touch /var/log/nginx/error.log"
 inside 1 root $local__container_name_lowercase "touch /var/log/nginx/run.log"
@@ -18,7 +17,7 @@ inside 1 root $local__container_name_lowercase "mkdir /etc/pki/nginx/private"
 inside 1 root $local__container_name_lowercase "echo -n '$local__nginx_config' > /etc/nginx/nginx.conf"
 ##
 ## Security
-inside 1 root $local__container_name_lowercase "openssl req -x509 -newkey rsa:4096 -sha512 -nodes -keyout /etc/pki/nginx/private/server.key -x509 -days 36500 -out /etc/pki/nginx/server.crt -subj '/C=XX/ST=Server/L=Server/O=Server/OU=Server/CN=Server'"
+inside 13 root $local__container_name_lowercase "openssl req -x509 -newkey rsa:4096 -sha512 -nodes -keyout /etc/pki/nginx/private/server.key -x509 -days 36500 -out /etc/pki/nginx/server.crt -subj '/C=XX/ST=Server/L=Server/O=Server/OU=Server/CN=Server'"
 inside 13 root $local__container_name_lowercase "htpasswd -cb5 /etc/nginx/.htpasswd layer_1 ${global__project_keys["[c:$local__container_name_lowercase]+[u:private]+[p:htpasswd]"]}"
 inside 1 root $local__container_name_lowercase "chown -c -R nginx:nginx /var/log/nginx"
 inside 1 root $local__container_name_lowercase "chmod -c -R 774 /var/log/nginx"
