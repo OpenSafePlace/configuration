79750c4120d086c15f0b0feac6e5540c08d0ca9a
Artur Shkadzinski <real-xihnik@yandex.by>
2023-12-06 22:48:20 +0300

    fix(types->neovim): formatting

--- .Bricks/Types/Neovim/actions/start.sh
+++ .Bricks/Types/Neovim/actions/start.sh
@@ -2,8 +2,9 @@
 
 # General process
 ## Start
-### Container #1
+##
+## Container #1
 local__container_name="Neovim"
-###
-### Configure firewall rules
+##
+## Configure firewall rules
 . $global__project_main_folder/.Bricks/Containers/Neovim/$global__project_os/$global__project_os_version/Firewall/Outgoing/Open/all.sh
