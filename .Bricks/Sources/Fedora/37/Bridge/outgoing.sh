240166ad979af2cc13edbaf72a5fdd52e9031f7c
Artur Shkadzinski <real-xihnik@yandex.by>
2023-12-30 13:34:38 +0300

    fix: improved the name of folders received from containers

--- .Bricks/Sources/Fedora/37/Bridge/outgoing.sh
+++ .Bricks/Sources/Fedora/37/Bridge/outgoing.sh
@@ -5,15 +5,15 @@
 local__container_name_lowercase="${local__container_name,,}"
 ##
 ## Remove unnecessary files
-sh -c "rm -rf $global__project_main_folder/Bridge/*"
+sh -c "rm -rf $global__project_main_folder/Bridge/${local__container_name_lowercase}-*"
 ##
 ## Send outgoing files
 inside 1 root $local__container_name_lowercase "mkdir -p /tmp/bridge/${local__datetime_iso}/${local__container_name}"
 inside 1 root $local__container_name_lowercase "cp -rf /bridge/outgoing/* /tmp/bridge/${local__datetime_iso}/${local__container_name}"
 
-podman cp ${global__project_name}-${local__container_name_lowercase}:/tmp/bridge/${local__datetime_iso}/${local__container_name} $global__project_main_folder/Bridge/${local__datetime_iso}
+podman cp ${global__project_name}-${local__container_name_lowercase}:/tmp/bridge/${local__datetime_iso}/${local__container_name} $global__project_main_folder/Bridge/${local__container_name_lowercase}-${local__datetime_iso}
 
-sh -c "mkdir -p $global__project_main_folder/Bridge/${local__datetime_iso}"
+sh -c "mkdir -p $global__project_main_folder/Bridge/${local__container_name_lowercase}-${local__datetime_iso}"
 ##
 ## Security
 sh -c "chmod 700 -R $global__project_main_folder/Bridge/*"
