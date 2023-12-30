4409e0a17a96cd566363791934c1bc2c84f8df1e
Artur Shkadzinski <real-xihnik@yandex.by>
2023-12-30 08:49:08 +0300

    feat(sources->f37): added the ability to interact with the vncserver

---
+++ .Bricks/Sources/Fedora/37/Protect/vncserver.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# General process
+## Vars
+local__container_name_lowercase="${local__container_name,,}"
+##
+## Set password
+inside 1 private $local__container_name_lowercase "vncpasswd -f > /home/private/.vnc/passwd <<EOF
+${global__project_keys_new[[c:$local__container_name_lowercase]+[u:private]+[p:vncserver]]}
+${global__project_keys_new[[c:$local__container_name_lowercase]+[u:private]+[p:vncserver]]}
+EOF"
+##
+info "В контейнере '$local__container_name' изменен основной пароль связанный с пакетом 'vncserver'"
